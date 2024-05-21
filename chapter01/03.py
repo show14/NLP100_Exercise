f5cc81e4e169a400fd0cd7bc02bb38abdf01f9fc
show14 <h.show0114@gmail.com>
2024-05-21 23:30:13 +0900

    update

--- chapter01/03.py
+++ chapter01/03.py
@@ -3,8 +3,8 @@
 “Now I need a drink, alcoholic of course, after the heavy lectures involving quantum mechanics.”という文を単語に分解し，各単語の（アルファベットの）文字数を先頭から出現順に並べたリストを作成せよ．
 '''
 str = "Now I need a drink, alcoholic of course, after the heavy lectures involving quantum mechanics."
-str = str.replace(",", "").replace(".", "")  # ,と.を除去
-splits = str.split()  # スペースで区切って単語ごとのリストを作成
+str = str.replace(",", "").replace(".", "")
+splits = str.split()
 ans = [len(i) for i in splits]
 
 print(ans)
