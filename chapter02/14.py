077fc46ba4268885fefc8a7294a8ab4597c723da
show14 <h.show0114@gmail.com>
2024-06-11 16:40:40 +0900

    initial commit

---
+++ chapter02/14.py
@@ -0,0 +1,23 @@
+'''
+14.先頭からN行を出力
+自然数Nをコマンドライン引数などの手段で受け取り，入力のうち先頭のN行だけを表示せよ．確認にはheadコマンドを用いよ．
+'''
+import sys
+
+def print_initial_lines(filename, n):
+    """ファイルから先頭のn行だけを表示する"""
+    
+    with open(filename, 'r') as file:
+        for i in range(n):
+            line = file.readline()
+            if not line:
+                break
+            print(line, end='')
+            
+filename = "popular-names.txt"  # ここでファイル名を直接指定
+n = int(sys.argv[1])
+print_initial_lines(filename, n)
+'''
+ターミナルの場合
+head -n N popular-names.txt
+'''
